a83c5822f02ba0f31b56b193ead1bac14f5c07d6
Anis <>
2025-12-03 10:30:04 +0200

    Add the Data object InventorySaveData script.

---
+++ Assets/_Project/Scripts/Features/Inventory/Data/InventorySaveData.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System;
+
+namespace Project.Features.Inventory.Data
+{
+    [Serializable]
+    public class InventorySaveData
+    {
+        public List<SlotSaveData> inventoryItems = new List<SlotSaveData>();
+        
+        [Serializable]
+        public struct SlotSaveData
+        {
+            public string itemID;
+            public int quantity;
+            public int slotIndex;
+        }
+    }
+}
