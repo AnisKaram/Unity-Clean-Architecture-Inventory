85d0bfc2ba5babfec53506e4db0c333dcacbd670
Anis <>
2025-12-08 20:24:47 +0200

    Add Player settings data with input reader script.

---
+++ Assets/_Project/Scripts/Features/Character/Data/PlayerSettingsSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Project.Features.Character.Data
+{
+    [CreateAssetMenu(fileName = "New PlayerInputSettings", menuName = "Character/PlayerInputSettings")]
+    public class PlayerSettingsSO : ScriptableObject
+    {
+        [field: SerializeField] public float movementSpeed { get; private set; } = 5f;
+        [field: SerializeField] public float jumpForce { get; private set; } = 5f;
+        [field: SerializeField] public float rotationSpeed { get; private set; } = 10f;
+    }
+}
