b1065d5e1ea81f86309ac2b9d492864ff0609a63
Anis <>
2025-12-12 16:53:22 +0200

    Add IdleAction.

---
+++ Assets/_Project/Scripts/Features/AI/Actions/IdleAction.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Project.Features.AI.Components;
+using Project.Features.AI.Domain;
+
+namespace Project.Features.AI.Actions
+{
+    public class IdleAction : UtilityActionBase
+    {
+        private readonly NpcContext m_Context;
+        
+        public IdleAction(NpcContext context, List<IConsideration> considerations) 
+            : base(considerations, "Idle Action")
+        {
+            m_Context = context;
+        }
+
+        public override void Execute()
+        {
+            m_Context.Agent.ResetPath();
+        }
+    }
+}
