6b8d915aac7dde8504cf3a09565c17f1f51875a9
Anis <>
2025-12-13 16:37:14 +0200

    Add summary to FixedConsideration script.

--- Assets/_Project/Scripts/Features/AI/Domain/FixedConsideration.cs
+++ Assets/_Project/Scripts/Features/AI/Domain/FixedConsideration.cs
@@ -1,5 +1,10 @@
 namespace Project.Features.AI.Domain
 {
+    /// <summary>
+    /// Represents only one fixed value consideration without using a curve.
+    /// This is helpful when we are using a fixed value for consideration, and it will
+    /// save CPU calculation.
+    /// </summary>
     public class FixedConsideration : IConsideration
     {
         public float Score { get; }
